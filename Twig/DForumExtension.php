9d890010ce3871e784ffb82fcfe2dc24a3ba764d
david verdier <david verdier>
2016-05-22 23:44:07 +0200

    Removed bbcodes for open source

--- Twig/DForumExtension.php
+++ Twig/DForumExtension.php
@@ -11,16 +11,10 @@ class DForumExtension extends \Twig_Extension
     private $em;
     
     private $request;
-    
-    private $params;
-    
-    private $prefixUrl;
-    
-    public function __construct (EntityManager $em, RequestStack $request, array $params, $prefixUrl) {
+
+    public function __construct (EntityManager $em, RequestStack $request) {
         $this->em = $em;
         $this->request = $request->getCurrentRequest();
-        $this->params = $params;
-        $this->prefixUrl = $prefixUrl;
     }
     
     public function getFunctions()
@@ -32,22 +26,8 @@ class DForumExtension extends \Twig_Extension
 
     public function urlsAlternateForum($forumId)
     {
-
-        $translations = $this->em->getRepository('DForumBundle:ForumTranslation')->findByTranslatable($forumId);
         
-        $translate = array();
-        
-        foreach ($translations as $translation){
-            if ( ( $translation->getLocale() != $this->request->getLocale() ) 
-                    && ( null !== $translation->getSlug() ) ) {
-                $locale = $translation->getLocale();
-                $slug = $translation->getSlug();
-                $link = '<link rel="alternate" hreflang="' . $locale . '" href="'.$this->prefixUrl . $this->params[$locale].'/forum/cat/'. $slug .'" />';
-               array_push($translate, $link);
-            }
-        }
-        
-        return $translate;
+        return '';
     }
 
     public function getName()
