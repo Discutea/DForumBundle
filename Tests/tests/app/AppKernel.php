82c2d8276a3ee066c7c88693237f9e8b8c2678dc
david verdier <david verdier>
2016-05-26 20:43:30 +0200

    [Travis] Updated dependancies

--- Tests/tests/app/AppKernel.php
+++ Tests/tests/app/AppKernel.php
@@ -11,7 +11,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
