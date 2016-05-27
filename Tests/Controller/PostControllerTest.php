f04297a1e2303e82d37e064d37fd2256d60c5c29
david verdier <david verdier>
2016-05-27 13:57:52 +0200

    Add test discutea_forum_post_delete

--- Tests/Controller/PostControllerTest.php
+++ Tests/Controller/PostControllerTest.php
@@ -15,13 +15,7 @@ class PostControllerTest extends TestBase
 
     public function testPostAction()
     {
-        $this->addFixtruresCategory();
-        $this->addFixtruresForum();
-        $this->addFixtruresTopic();
-        $this->addFixtruresPost();
-        
-        $topics = $this->em->getRepository('DForumBundle:Topic')->findAll();
-        $this->assertCount(4, $topics);
+        $topics = $this->addFixturesAndFindTopics();
         
         foreach ($topics as $topic) {
             $slug = $topic->getSlug();
@@ -40,5 +34,52 @@ class PostControllerTest extends TestBase
             }
            $this->assertTrue($this->adminCrawler->filter('html:contains("replyPost")')->count() == 1 );
         }
+    }
+
+    public function testDeleteAction()
+    {
+        $topics = $this->addFixturesAndFindTopics();
+        
+        $i = 0;
+        
+        foreach ($topics as $topic) {
+            $url = $this->client->getContainer()->get('router')->generate('discutea_forum_post_delete', array('id' => $topic->getId()));
+            $posts = $topic->getPosts();
+            $categoryName = $topic->getForum()->getCategory()->getName();
+            foreach ($posts as $post) {
+                if ($categoryName = 'adminCategoryTest') {
+                    $this->tryUrl(302, 403, 403, 403, 302, $url);
+                    $this->tryUrl(404, 404, 404, 404, 404, $url);
+                } elseif ($categoryName = 'moderatorCategoryTest') {
+                    $this->tryUrl(302, 403, 403, 302, 404, $url);
+                    $this->tryUrl(404, 404, 404, 404, 404, $url);
+                } else {
+                    if ($i === 0) {
+                        $this->client = $this->doLogin('admin', 'password');
+                        $this->adminCrawler = $this->client->request('GET', $url);
+                        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+                        $this->tryUrl(404, 404, 404, 404, 404, $url);
+                    } else {
+                        $this->tryUrl(302, 403, 403, 302, 404, $url);
+                        $this->tryUrl(404, 404, 404, 404, 404, $url); 
+                    }
+                    $i++;
+                }
+            }
+        }
+        
+    }
+    
+    private function addFixturesAndFindTopics()
+    {
+        $this->addFixtruresCategory();
+        $this->addFixtruresForum();
+        $this->addFixtruresTopic();
+        $this->addFixtruresPost();
+
+        $topics = $this->em->getRepository('DForumBundle:Topic')->findAll();
+        $this->assertCount(4, $topics);
+        
+        return $topics;
     }
 }
